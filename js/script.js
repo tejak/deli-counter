85868748221c1842f7c9174d3b713b082cdb066d
mchuchul <matthewchuchul@gmail.com>
2015-07-21 12:06:00 +0000

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+$(document).ready(function(){
+                  
+
+// part1: add a new person's name to the line
+                  
+                  
+                  
+// part1: notify the person of their position on the line
+                  
+   
+                  
+// part2: notify the person who is next on the line   
+                  
+                  
+                  
+// part2: remove the person who was just served from the line
+      
+                  
+                  
+// part2/extension: notify if there is no one on the line
+
+                  
+
+// part2/extension: notify the person if there is no one on the line 
+                
+                  
+
+// part3: when someone gives a name, notify the person of the position in line
+
+  
+
+                  
+                
+                  
+                  
+
+                  
+});
