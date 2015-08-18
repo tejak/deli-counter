0d800afe40dcc793467db4c7918f44787ba39546
tejak <teja.kondapalli@yahoo.com>
2015-08-18 16:11:13 +0000

    modifications to js - jquery scaffolding

--- js/script.js
+++ js/script.js
@@ -1,38 +1,45 @@
 $(document).ready(function(){
                   
 
-// part1: add a new person's name to the line
-                  
-                  
-                  
-// part1: notify the person of their position on the line
-                  
-   
-                  
+// declare an array that represents your deli counter
+  
+// part1: add a new person's name to the line                  
+// write a function add that takes in a person's name and adds them to the list
+// it should return the person's position in line
+var add;
+  
 // part2: notify the person who is next on the line   
-                  
-                  
+// write a function next that returns who is next to be serviced
+var next; 
                   
 // part2: remove the person who was just served from the line
-      
-                  
+// write a function serve that serves the next person in line
+var serve;
                   
 // part2/extension: notify if there is no one on the line
-
-                  
-
-// part2/extension: notify the person if there is no one on the line 
-                
-                  
+// do this in the function next and serve... if there is no one in line alert !
 
 // part3: when someone gives a name, notify the person of the position in line
-
+// write a function getPositon that takes in someone's name and returns their position in line
+var getPosition; 
   
 
-                  
-                
-                  
-                  
+// OUR CODE DO NOT TOUCH
+$("#add").click(function(){
+  position = add($("#addname").val());
+  $("#addnumber").html(position);
+});
+  
+$("#next").click(function(){
+  $("#nexttext").html(next());
+});             
 
-                  
+$('#search').click(function () {
+  $("#position").html(getPosition($("#nameinput").val()));
+});
+  
+$("#serve").click(function(){
+  serve();
+});   
+                                  
 });
